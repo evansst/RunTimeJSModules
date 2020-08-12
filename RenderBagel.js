ee3397cc42645817f3e7df5b29345d970fd93a09
Jessica Pope <jessica.pope.co@gmail.com>
2020-08-12 14:34:45 -0600

    August Class Sample

---
+++ RenderBagel.js
@@ -0,0 +1,11 @@
+const bagelUl = document.getElementById('bagel-ul')
+
+function renderBagelType(bagel, id){
+    const li = document.createElement('li')
+    li.innerText = bagel
+    li.id = id
+    bagelUl.appendChild(li)
+    createDeleteButton(li)
+}
+
+export {renderBagelType}
